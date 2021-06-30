c73e43024462b852b7ac9d63c489489642256843
aishcann <aish.cann@gmail.com>
2021-06-30 17:54:38 -0400

    functionality and styling on HomePage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,31 @@ import HomePage from './components/HomePage/HomePage';
 import EventDetail from './components/EventDetail/EventDetail';
 import LoginPage from './components/LoginPage/LoginPage';
 import ManageEvents from './components/ManageEvents/ManageEvents';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="app">
-     hello from app
-     <HomePage/>
-     <EventDetail/>
-     <LoginPage/>
-     <ManageEvents/>
-    </div>
-  );
+		<div className='app'>
+			<Route
+				path='/'
+				exact
+				component={HomePage}
+				render={(routerProps) => <HomePage match={routerProps.match} />}
+			/>
+			<Route
+				path='/event/:id'
+				render={(routerProps) => <EventDetail match={routerProps.match} />}
+			/>
+			<Route
+				path='/login'
+				render={(routerProps) => <LoginPage match={routerProps.match} />}
+			/>
+			<Route
+				path='/account'
+				render={(routerProps) => <ManageEvents match={routerProps.match} />}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,11 +1,38 @@
 import './homepage.scss'
+import eventData from '../../data.json'
+import { Link } from 'react-router-dom' 
 
-const HomePage = () => {
+const HomePage = ({data}) => {
+
+    console.log(data)
     return (
-        <div>
-            Hello from HomePage
-        </div>
-    );
+			<div className='homePage'>
+				<section className='navbar'>
+					<Link to='/login' >
+                        <button>Artist Login</button>
+                    </Link>
+				</section>
+				<section className='header'>
+					<h1>In Your City</h1>
+					<h3>the latest cultural events near you</h3>
+				</section>
+				<section className='eventList'>
+					{eventData.map((event) => {
+						return (
+							<div className='eventInfo'>
+								<img src={event.photo} alt={event.name} />
+								<h3>{event.name}</h3>
+								<h4>{event.date}</h4>
+								<h4>{event.time}</h4>
+								<h4>{event.location}</h4>
+								<h4>{event.link}</h4>
+								<p>{event.description}</p>
+							</div>
+						);
+					})}
+				</section>
+			</div>
+		);
 };
 
 export default HomePage;
